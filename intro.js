5c56a6f6365edbf67ed7a2033499cf19e35e0cb7
Raul Bogdan <raulmorarbogdan@gmail.com>
2019-03-11 21:02:26 +0200

    cleanup js intro

--- intro.js
+++ intro.js
@@ -1,50 +1,9 @@
-console.info("my first js file");
-
-var name="Raul";
-var employed=false;
-
-console.warn("employed");
-console.warn(employed);
-
-console.info("name");
-console.info(name);
-
-name="Bogdan";
-
-console.info("name");
-console.info(name);
-
-console.log("before welcome message");
 function getWelcomeMSG() {
     console.log("inside welcome message");
     return "Welcome to my site";
 }
 console.log("after welcome message");
 
-function getColor() {
-    var date = new Date();
-    console.log(date.getMinutes());
-    if (date.getMinutes() > 16) {
-        return "green"
-    }
-    return "red";
-}
-// first example
-function hideHomePage() {
-   var el = document.getElementById("home-page");
-   el.style.display= "none";
-}
-function showSkillsPage() {
-    document.getElementById("skills-page").style.display="block";
-}
-function clickOnSkills(){
-   // hideHomePage();
-   // showSkillsPage();
-   hidePage("home-page");
-   showPage("skills-page");
-
-}
-//first example end
 var welcome=getWelcomeMSG();
 console.info(welcome);
 
@@ -53,8 +12,6 @@ console.info("summaryElement", summaryElement)
 var color="red";
 summaryElement.style.color="color"
 
-//summaryElement.style.color = "blue";
-
 function hidePage(page) {
     var el = document.getElementById(page);
     el.style.display= "none";
@@ -62,9 +19,3 @@ function hidePage(page) {
  function showPage(page) {
      document.getElementById(page).style.display="block";
  }
-function clickOnSkills(){
-    hidePage("home-page");
-    hidePage("languages-page");
-    hidePage("about-page");
-    showPage("skills-page");
-}
