9b60b95df5f5240a28cdba2d316c50cf34d39267
Raul Bogdan <raulmorarbogdan@gmail.com>
2019-03-25 20:55:33 +0200

    sort by endorsements

--- intro.js
+++ intro.js
@@ -1,64 +1,61 @@
 var summaryElement = document.getElementById("summary")
-var color="red";
-summaryElement.style.color="color"
+var color = "red";
+summaryElement.style.color = "color"
 
- function hidePage(page) {
+function hidePage(page) {
     var el = document.getElementById(page);
-    el.style.display= "none";
- }
- function showPage(page) {
-     document.getElementById(page).style.display="block";
- }
+    el.style.display = "none";
+}
+function showPage(page) {
+    document.getElementById(page).style.display = "block";
+}
 
- function initMenu() {
-     var links = document.querySelectorAll('#top-menu-bar a')
-    console.info(links); 
-    for(var i = 0; i < links.length; i++){
+function initMenu() {
+    var links = document.querySelectorAll('#top-menu-bar a')
+    console.info(links);
+    for (var i = 0; i < links.length; i++) {
         links[i].onclick = clickOnMenuItem;
     }
- }
+}
 
- function   clickOnMenuItem() {
-         console.warn('clicked on menu',this);
-         hideAllPages();
-         var pageid = this.getAttribute('data-page');
-         console.warn({pageid});
-         showPage(pageid);
- }
+function clickOnMenuItem() {
+    console.warn('clicked on menu', this);
+    hideAllPages();
+    var pageid = this.getAttribute('data-page');
+    console.warn({ pageid });
+    showPage(pageid);
+}
 
- function hideAllPages() {
-     var pages = document.querySelectorAll('.page');
-     for(var i =0; i < pages.length; i++){
-         pages[i].style.display= 'none'
-     }
- }
- initMenu();
- 
- function showSkills() {
-     var skills = [
-         {name: 'html', endorsements: 8, endorsedBy: 'Andrei I'},
-         {name: 'css', endorsements: 12},
-         {name: 'javascript', endorsements: 20, endorsedBy: 'Nicu A.'},
-         {name: 'nodejs', endorsements: 3}
-        ];
+function hideAllPages() {
+    var pages = document.querySelectorAll('.page');
+    for (var i = 0; i < pages.length; i++) {
+        pages[i].style.display = 'none'
+    }
+}
+initMenu();
 
-     var htmlSkills = skills.map(function(skill) {
-         var endorsedBy = skill.endorsedBy ? ` - ` + skill.endorsedBy : '';
-        // var endorsedBy = skill.endorsedBy;
-        // if (endorsedBy) {
-        //    endorsedBy = ' - ' + endorsedBy;
-        // } else {
-        //     endorsedBy = '';
-        //}
-         var endorsements =`<span class="endorsement">(${skill.endorsements})${endorsedBy})</span>`;
+function showSkills() {
+    var skills = [
+        { name: 'html', endorsements: 8, endorsedBy: 'Andrei I' },
+        { name: 'css', endorsements: 12 }, 
+        { name: 'javascript', endorsements: 20, endorsedBy: 'Nicu A.' },
+        { name: 'nodejs', endorsements: 3 }
+    ];
+    skills.sort(function(a, b){
+        return b.endorsements - a.endorsements;
+    });
+
+    var htmlSkills = skills.map(function (skill) {
+        var endorsedBy = skill.endorsedBy ? ` - ` + skill.endorsedBy : '';
+        var endorsements = `<span class="endorsement">(${skill.endorsements})${endorsedBy}</span>`;
         return '<li>' + skill.name.toUpperCase() + endorsements + '</li>';
     });
-       
-     var ul = document.querySelector('#skills-page ul');
-      ul.innerHTML = htmlSkills.join('');
-       
+
+    var ul = document.querySelector('#skills-page ul');
+    ul.innerHTML = htmlSkills.join('');
+
 }
 hideAllPages();
 showPage('skills-page');
- showSkills();
+showSkills();
 
